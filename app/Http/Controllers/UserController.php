b48e944f102e68d4b96b0a036663465cfa524bc2
Sothoun-Phauv <sothoun.phauv@student.passerellesnumeriques.org>
2021-10-27 11:18:28 +0700

    homework-laravel

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+    public function register(Request $request){
+        //
+        $user = new User();
+        $user ->name = $request->name;
+        $user ->email = $request->email;
+        $user ->password = $request->password;
+        $user ->save();
+        return respone()->json(['Message'=>'Created'],201);
+
+    }
+    public function login(Request $request)
+    {
+        # code...
+
+    }
+    public function logout(Request $request)
+    {
+        # code...
+    }
+}
